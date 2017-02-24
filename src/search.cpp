92be32f3882153e878e15fe5dc4d6064b1ff8f99
IIvec <ivan.ivec@gmail.com>
2017-02-24 23:45:27 +0100

    CorChess b3

--- src/search.cpp
+++ src/search.cpp
@@ -78,7 +78,7 @@ namespace {
   // History and stats update bonus, based on depth
   Value stat_bonus(Depth depth) {
     int d = depth / ONE_PLY ;
-    return Value(int(420 * d * (1.0 - exp(-8.5 / d))));
+    return Value(d * d + 2 * d - 2);
   }
 
   // Skill structure is used to implement strength limit
