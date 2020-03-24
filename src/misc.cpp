3c6d8454c513c17984155da022a1ac4d9ce74ebd
IIvec <ivan.ivec@gmail.com>
2020-03-24 14:07:31 +0100

    Tuned values

--- src/misc.cpp
+++ src/misc.cpp
@@ -139,7 +139,7 @@ const string engine_info(bool to_uci) {
   string month, day, year;
   stringstream ss, date(__DATE__); // From compiler, format is "Sep 21 2008"
 
-  ss << "CorChess 5.2 " << Version << setfill('0');
+  ss << "CorChess 6.0 " << Version << setfill('0');
 
   if (Version.empty())
   {
